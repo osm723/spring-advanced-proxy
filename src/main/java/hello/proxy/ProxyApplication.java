af475244c5cb70d87ba41e6bac3718d33fcbd327
Oh <osm723@gmail.com>
2024-10-09 15:08:13 +0900

    스프링 핵심 원리 - 고급편 섹션 9. @Aspect AOP 정리

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,9 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoprxoy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +24,10 @@ import org.springframework.context.annotation.Import;
 //@Import({DynamicProxyBasicConfig.class, ConcreteProxyConfig.class})
 //@Import({DynamicProxyFilterConfig.class, ConcreteProxyConfig.class})
 //@Import({ProxyFactoryConfigV1.class, ConcreteProxyConfig.class})
-@Import({ProxyFactoryConfigV2.class})
+//@Import({ProxyFactoryConfigV2.class})
+//@Import({BeanPostProcessorConfig.class})
+//@Import({AutoProxyConfig.class})
+@Import({AopConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
